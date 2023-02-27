53e0c6dd92a851540fb9de1c15c5f52e4460aa76
Sena Uzlas <senauzlas04@gmail.com>
2023-02-27 16:06:08 +0300

    v3

--- src/main/java/DortIslem.java
+++ src/main/java/DortIslem.java
@@ -1,10 +1,15 @@
-public class Toplama {
+public class DortIslem {
 
     public static void main(String[] args) {
         toplama(10,25);
+        cikarma(25,5);
     }
 
     public static void toplama(int a, int b){
         System.out.println(a+b);
+    }
+
+    public static void cikarma(int a, int b){
+        System.out.println(a-b);
     }
 }
