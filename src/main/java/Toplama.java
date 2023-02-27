b8071b3f97f59144b4db841b6b6231b65c949045
Sena Uzlas <senauzlas04@gmail.com>
2023-02-27 15:54:25 +0300

    v1

---
+++ src/main/java/Toplama.java
@@ -0,0 +1,10 @@
+public class Toplama {
+
+    public static void main(String[] args) {
+        toplama(10,25);
+    }
+
+    public static void toplama(int a, int b){
+        System.out.println(a+b);
+    }
+}
